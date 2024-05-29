d15a723d903f071d1377572f7d518a1d1b399656
akashguptawebdev <akashgupta.webdev@gmail.com>
2024-05-29 16:24:09 +0530

    Start With

---
+++ StartWithJava.js
@@ -0,0 +1,14 @@
+let str = "jaawelomwjava";
+
+const prefix = "java";
+
+ function  CheckStartWith(){
+    if (str.startsWith(prefix)) {
+        return true
+     } else {
+         return false;
+     }
+}
+
+
+console.log(CheckStartWith())
